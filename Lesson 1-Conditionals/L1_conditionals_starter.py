8dbdea9635d8aa3d0e29021db2f24c2851def60a
Gabriella Shaka <gabsha28@gmail.com>
2025-09-17 09:00:34 -0400

    c

---
+++ Lesson 1-Conditionals/L1_conditionals_starter.py
@@ -0,0 +1,46 @@
+# ========================================
+# ACCELERATED PYTHON UNIT 2 - LESSON 1
+# Conditionals: JavaScript to Python
+# ========================================
+
+# ========================================
+# SECTION 1: QUICK TRANSLATION CHALLENGE
+# ========================================
+print("==GRADE CLASSIFICATION==")
+score = 87
+if score >= 90:
+    print("A grade!")
+elif score >= 80:
+    print("B grade!")
+else:
+    print("Below B")
+
+# ========================================
+# SECTION 2: AGE CATEGORY CLASSIFIER
+# ========================================
+age = input("Enter your age: ")
+if (0 <= age <= 12):
+    print("You are a child")
+elif (13 <= age <= 19):
+    print("You are a teenager")
+elif (20 <= age <= 64):
+    print("You are an adult")
+elif (age >= 65):
+    print("You are a senior")
+else:
+    print("Please enter a valid age!")
+
+
+# ========================================
+# SECTION 3: STUDENT STATUS CHECKER
+# ========================================
+
+
+# ========================================
+# SECTION 4: GRADE VALIDATOR CHALLENGE
+# ========================================
+
+
+# ========================================
+# SECTION 5: WEATHER DECISION SYSTEM
+# ========================================
