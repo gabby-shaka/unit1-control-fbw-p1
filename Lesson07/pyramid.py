b223943725d6f9642ea72feee7ca022144f23dc9
Gabriella Shaka <gabsha28@gmail.com>
2025-10-09 08:56:22 -0400

    10/9/2025

---
+++ Lesson07/pyramid.py
@@ -0,0 +1,10 @@
+#Pyrimad Pattern
+
+# STEP-BY-STEP GUIDE:
+# Row O: 4 spaces, 1 star
+# Row 1: 3 spaces, 3 stars
+# Row 2: 2 spaces 5 stars
+# Row 3: 1 space, 7 stars
+# Row 4: 0 spaces, 9 stars
+
+rows = 5
