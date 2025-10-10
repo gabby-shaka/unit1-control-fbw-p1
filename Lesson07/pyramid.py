695a74260e35467b80c0557416529fd0276927db
Gabriella Shaka <gabsha28@gmail.com>
2025-10-10 08:38:25 -0400

    10/10/2025

--- Lesson07/pyramid.py
+++ Lesson07/pyramid.py
@@ -8,3 +8,14 @@
 # Row 4: 0 spaces, 9 stars
 
 rows = 5
+#create an outer loop for the rows
+#step 1: create an 
+for i in range(rows):
+    #step 2
+    for j in range(rows-i-1):
+        print(" ", end="")
+        #step 3
+        for k in range(2*i+1):
+            print("*", end="")
+            #4
+            print()
